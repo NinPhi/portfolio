1cee5d8e153ea2cbbdf354d3d1c030fcdd4699f2
NinPhi <melmarka4@gmail.com>
2024-03-30 20:34:50 +0600

    added rough layout for portfolio view

--- front-src/src/router/index.ts
+++ front-src/src/router/index.ts
@@ -1,8 +1,15 @@
+import PortfolioView from "@/views/PortfolioView.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      name: "portfolio",
+      component: PortfolioView,
+    },
+  ],
 });
 
 export default router;
---
+++ front-src/src/views/PortfolioView.vue
@@ -0,0 +1,109 @@
+<script setup lang="ts">
+import SocialMedia from "../components/SocialMedia.vue";
+</script>
+
+<template>
+  <div class="container">
+    <div class="general">
+      <h1>Name Surname</h1>
+      <h2>Fullstack Software Engineer</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+      <div class="socials">
+        <SocialMedia />
+        <SocialMedia />
+        <SocialMedia />
+        <SocialMedia />
+        <SocialMedia />
+      </div>
+    </div>
+    <section id="about">
+      <div class="about">
+        <h2>About</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga porro
+          ducimus repellendus explicabo velit natus dolorem dignissimos sint!
+          Odit adipisci accusamus culpa expedita voluptate!
+        </p>
+        <p>
+          Vero assumenda qui est omnis quod! Illo consequatur beatae modi odit
+          minima quo at reiciendis quis. Accusantium, odit vel. Cumque similique
+          veniam id sit earum. Nihil omnis inventore iusto iste? Ad ullam iure
+          doloremque. Facere, quasi. Architecto omnis est qui hic quasi, culpa
+          suscipit deleniti soluta sunt consequatur perspiciatis quidem non
+          labore magni enim eius odio iste magnam nobis nesciunt!
+        </p>
+        <p>
+          A dolorem magni quod tempora autem! Laborum necessitatibus dignissimos
+          facilis ut animi, obcaecati molestias esse officia commodi adipisci
+          consequatur tenetur vel fuga nobis quis iure quasi at ad amet sed?
+        </p>
+      </div>
+    </section>
+    <section id="experience">
+      <div class="experience">
+        <h2>Experience</h2>
+        <div class="experience-list">
+          <div class="experience-card">
+            <p>Mar 2023 - Present</p>
+            <span>Position - Company</span>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+              tempore repudiandae veritatis doloremque! Tempore quas iusto
+              perferendis eius minima praesentium eos provident, voluptatem
+              fugiat ea officia odio accusantium blanditiis doloremque.
+            </p>
+          </div>
+          <div class="experience-card">
+            <p>Mar 2023 - Present</p>
+            <span>Position - Company</span>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+              tempore repudiandae veritatis doloremque! Tempore quas iusto
+              perferendis eius minima praesentium eos provident, voluptatem
+              fugiat ea officia odio accusantium blanditiis doloremque.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="projects">
+      <div class="projects">
+        <h2>Projects</h2>
+        <div class="projects-list">
+          <div class="project-card">
+            <div class="thumbnail"></div>
+            <span>Project Title</span>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Doloribus est ab cupiditate explicabo deleniti blanditiis
+              corporis! Aliquam dolore aspernatur dolores quam, voluptatem atque
+              repudiandae adipisci fugiat, iure dolor iste harum.
+            </p>
+          </div>
+          <div class="project-card">
+            <div class="thumbnail"></div>
+            <span>Project Title</span>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Doloribus est ab cupiditate explicabo deleniti blanditiis
+              corporis! Aliquam dolore aspernatur dolores quam, voluptatem atque
+              repudiandae adipisci fugiat, iure dolor iste harum.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.socials {
+  display: flex;
+}
+
+.thumbnail {
+  width: 200px;
+  height: 120px;
+  background-color: black;
+}
+</style>
